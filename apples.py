610a9725ab2c166a782af7d8c05ed4af0a7507e4
Ingrid <ingrid@lambdarecruiting.com>
2017-10-29 00:43:04 +0100

    Following tutorial
    Playing with types and functions

--- apples.py
+++ apples.py
@@ -2,10 +2,51 @@ import random
 import sys
 import os
 
-test_file = ("test.txt", "wb")
+alist = ['apple', 'bananas', 'cherries', 'tomatoes']
 
-print(test_file.mode)
+a = 5
 
-test_file.write(bytes("I am writing this to a file.\n", "UTF-8"))
+print(5 + 5)
 
-test_file.close()
+print(alist[3])
+
+print(a)
+
+print(a + 5)
+
+
+def bar(x):
+    if x == 4:
+        print('ta da')
+    else:
+        print('nope')
+
+
+def foo(x):
+    while x >= 0:
+        print(x)
+        x = x - 1
+        if x == 0:
+            bar(x)
+    print('done')
+
+
+
+bar(4)
+foo(5)
+
+num = 10
+
+print('I have just printed %s pages' % (num))
+
+print(f'I am testing this cool thign {num}')
+
+print("""Dear %(recipient)s,
+
+I wish you to leave Sunnydale and never return.
+
+Not Quite Love,
+%(sender)s
+""" % {'sender': 'Buffy the Vampire Slayer', 'recipient': 'Spike'})
+
+
